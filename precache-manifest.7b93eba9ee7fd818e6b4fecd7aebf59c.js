7cb7886b1b9778172a667b34d8ab1c606b498c0f
Andrei Pasat <pasat_andrei@yahoo.com>
2020-06-14 13:23:32 +0300

    Updates

---
+++ precache-manifest.7b93eba9ee7fd818e6b4fecd7aebf59c.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca01d4d479cbbfd6ed64d77e4d03404",
+    "url": "/react-social/index.html"
+  },
+  {
+    "revision": "1ee7ecea0ed5fcdfd1b3",
+    "url": "/react-social/static/css/4.63cd208d.chunk.css"
+  },
+  {
+    "revision": "9605c79a00550a7f6a4c",
+    "url": "/react-social/static/css/5.930f0256.chunk.css"
+  },
+  {
+    "revision": "22c1ad837d5dd14d9eb0",
+    "url": "/react-social/static/css/6.a22c6f3e.chunk.css"
+  },
+  {
+    "revision": "f10fe164f592f345598d",
+    "url": "/react-social/static/css/7.d2e1599e.chunk.css"
+  },
+  {
+    "revision": "81b028b376ba3cf4d935",
+    "url": "/react-social/static/css/main.d10c8e4d.chunk.css"
+  },
+  {
+    "revision": "6a5a64b69bc0973ff5e9",
+    "url": "/react-social/static/js/0.64136924.chunk.js"
+  },
+  {
+    "revision": "9494871312d1384ae6e9",
+    "url": "/react-social/static/js/3.8a06e480.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-social/static/js/3.8a06e480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ee7ecea0ed5fcdfd1b3",
+    "url": "/react-social/static/js/4.6eff01ea.chunk.js"
+  },
+  {
+    "revision": "9605c79a00550a7f6a4c",
+    "url": "/react-social/static/js/5.6de298d9.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/react-social/static/js/5.6de298d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c1ad837d5dd14d9eb0",
+    "url": "/react-social/static/js/6.72d96dc2.chunk.js"
+  },
+  {
+    "revision": "f10fe164f592f345598d",
+    "url": "/react-social/static/js/7.bf6c5b07.chunk.js"
+  },
+  {
+    "revision": "81b028b376ba3cf4d935",
+    "url": "/react-social/static/js/main.612dec98.chunk.js"
+  },
+  {
+    "revision": "b0eeac65958b33d2799d",
+    "url": "/react-social/static/js/runtime-main.18dfff68.js"
+  },
+  {
+    "revision": "e4b6323e19e57f6761b1ce47b7d329e0",
+    "url": "/react-social/static/media/loading.e4b6323e.gif"
+  },
+  {
+    "revision": "6d06f0be373527d560e3398e9efceaa8",
+    "url": "/react-social/static/media/user_icon.6d06f0be.png"
+  }
+]);
